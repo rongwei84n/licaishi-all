3e5a3c312a03f5d268b66ee0281cec18e5286462
黄荣卫 <rongwei.huang@phicomm.com>
2018-01-27 21:36:32 +0800

    修改编译错误

--- licaishi-server/backend/licaishi-server/src/main/java/com/phicomm/smarthome/authservice/controller/ProductsController.java
+++ licaishi-server/backend/licaishi-server/src/main/java/com/phicomm/smarthome/authservice/controller/ProductsController.java
@@ -1,24 +1,15 @@
 package com.phicomm.smarthome.authservice.controller;
 
-import com.alibaba.fastjson.JSON;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
-import com.phicomm.smarthome.authservice.model.common.PhicommAccountDetailModel;
+import com.phicomm.smarthome.authservice.consts.Const;
+import com.phicomm.smarthome.authservice.model.common.PhiHomeBaseResponse;
 import com.phicomm.smarthome.authservice.model.common.PhicommServerConfigModel;
-import com.phicomm.smarthome.authservice.model.request.InnerAuthPhicommTokenModel;
-import com.phicomm.smarthome.authservice.util.PhicommHttpsClient;
-import com.phicomm.smarthome.authservice.util.StringUtil;
-import com.phicomm.smarthome.consts.PhihomeConst.ResponseStatus;
+import com.phicomm.smarthome.consts.PhihomeConst;
 
-import com.phicomm.smarthome.util.MyResponseutils;
-
-import java.util.HashMap;
-import java.util.Map;
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @author rongwei.huang
  */
 @RestController
-public class ProductsController {
+public class ProductsController extends SBaseController {
     private static final Logger LOGGER = LogManager.getLogger(ProductsController.class);
 
     public static final String DEFAULT_CHARSET = "utf-8";
@@ -41,42 +32,19 @@ public class ProductsController {
     /**
      * 查询产品.
      */
-    @RequestMapping(value = "/app/products", method = RequestMethod.POST, produces = { "application/json" })
-    public PhicommAccountDetailModel authPhicommToken(@RequestBody InnerAuthPhicommTokenModel requestParas) {
-        if (requestParas == null) {
-            LOGGER.info("no request params");
-            return errorResponse(ResponseStatus.STATUS_NO_PARA_IN_REQUEST);
+    @RequestMapping(value = "/server/hot_products", method = RequestMethod.GET, produces = { "application/json" })
+    public PhiHomeBaseResponse hotProducts(HttpServletRequest request) {
+        PhiHomeBaseResponse rspObj = new PhiHomeBaseResponse();
+        String token = request.getHeader(PhihomeConst.AUTHORIZATION);
+        // 通过token获取账户uid
+        String uid = null;
+        Object object = getUIDByToken(token, rspObj);
+        if (object instanceof String) {
+            uid = (String) object;
+        } else {
+            return (PhiHomeBaseResponse) object;
         }
 
-        if (StringUtil.isNullOrEmpty(requestParas.getToken())) {
-            LOGGER.info("cannot find request token for this reqeust");
-            return errorResponse(ResponseStatus.STATUS_NO_PARA_IN_REQUEST);
-        }
-
-        return errorResponse(ResponseStatus.STATUS_NO_PARA_IN_REQUEST);
-    }
-
-    private PhicommAccountDetailModel errorResponse(int code) {
-        PhicommAccountDetailModel model = new PhicommAccountDetailModel();
-        model.setTokenStatus(String.valueOf(code));
-        model.setMessage(MyResponseutils.parseMsg(code));
-        return model;
-    }
-
-    private PhicommAccountDetailModel getPhicommAccountByToken(String phicommCloudServer, String token) {
-        Map<String, String> headerParas = new HashMap<String, String>(1);
-        headerParas.put(HTTP_HEAD_AUTHORIZATION, token);
-        try {
-            String accountJson = PhicommHttpsClient.httpsGet(phicommCloudServer,
-                    DEFAULT_CHARSET,
-                    headerParas);
-            LOGGER.info("accountJson [{}]", accountJson);
-            if (StringUtil.isNotEmpty(accountJson)) {
-                return JSON.parseObject(accountJson, PhicommAccountDetailModel.class);
-            }
-        } catch (Exception e) {
-            LOGGER.error("get phicomm account error: ", e);
-        }
-        return null;
+        return errorResponse(Const.STATUS_NO_PARA_IN_REQUEST);
     }
 }
---
+++ licaishi-server/backend/licaishi-server/src/main/java/com/phicomm/smarthome/authservice/controller/SBaseController.java
@@ -0,0 +1,142 @@
+package com.phicomm.smarthome.authservice.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.netflix.discovery.EurekaClient;
+import com.phicomm.smarthome.authservice.model.common.PhiHomeBaseResponse;
+import com.phicomm.smarthome.authservice.model.common.PhicommAccountDetailModel;
+import com.phicomm.smarthome.authservice.util.PhicommHttpsClient;
+import com.phicomm.smarthome.consts.PhihomeConst.ResponseStatus;
+import com.phicomm.smarthome.util.MyResponseutils;
+import com.phicomm.smarthome.util.StringUtil;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Phihome Controller基类, 提供了一些返回的基本功能.
+ *
+ * @author huangrongwei
+ *
+ */
+public abstract class SBaseController {
+
+    private static final String PHICOMM_ACCOUNT_URL = "https://accountsym.phicomm.com";
+
+    public static final String DEFAULT_CHARSET = "utf-8";
+    public static String HTTP_HEAD_AUTHORIZATION = "Authorization";
+    public static String HTTP_HEAD_CONTENT_TYPE = "Content-Type";
+
+    private static Logger logger = LogManager.getLogger(SBaseController.class);
+
+    private RestTemplate restTemplate = new RestTemplate();
+
+    @Autowired
+    private EurekaClient discoveryClient;
+
+    public static PhiHomeBaseResponse geResponse(Object result) {
+        PhiHomeBaseResponse smartHomeResponseT = new PhiHomeBaseResponse();
+        smartHomeResponseT.setResult(result);
+        return smartHomeResponseT;
+    }
+
+    protected PhiHomeBaseResponse errorResponse(int errCode) {
+        String errMsg = MyResponseutils.parseMsg(errCode);
+        return errorResponse(errCode, errMsg);
+    }
+
+    protected PhiHomeBaseResponse errorResponse(int errCode, String errMsg) {
+        PhiHomeBaseResponse response = geResponse(null);
+        response.setCode(errCode);
+        if (StringUtil.isNullOrEmpty(errMsg)) {
+            response.setMessage(MyResponseutils.parseMsg(errCode));
+        } else {
+            response.setMessage(errMsg);
+        }
+        return response;
+    }
+
+    protected PhiHomeBaseResponse successResponse(Object obj) {
+        PhiHomeBaseResponse response = (PhiHomeBaseResponse) obj;
+        response.setCode(ResponseStatus.STATUS_OK);
+        response.setMessage(MyResponseutils.parseMsg(ResponseStatus.STATUS_OK));
+        return response;
+    }
+
+    protected Object getUIDByToken(String token, Object object) {
+        logger.info("token: " + token);
+        if (StringUtil.isNullOrEmpty(token)) {
+            logger.error("No token in request");
+            return errorResponse(ResponseStatus.STATUS_NO_TOKEN_IN_REQUEST);
+        }
+        token = token.replace("Bearer ", "");
+        // 通过token获取斐讯云uid
+        PhicommAccountDetailModel parsedObj = getPhicommAccountByToken(token);
+        if (parsedObj == null) {
+            logger.error("Failed to get phicomm account parsedObj null");
+            return errorResponse(ResponseStatus.STATUS_FAILED_GET_PHICOMM_ACCOUNT);
+        }
+        if (StringUtil.equals(parsedObj.getError(), "5")) {
+            logger.error("token is invalid.");
+            return errorResponse(ResponseStatus.STATUS_TOKEN_INVALID);
+        } else if (StringUtil.equals(parsedObj.getError(), "30")) {
+            logger.error("account logined by other phone.");
+            String reason = String.format("{\"reason\":\"%s\"}", parsedObj.getReason());
+            Object obj = JSON.parse(reason);
+            PhiHomeBaseResponse response = errorResponse(ResponseStatus.STATUS_LOGIN_BY_OTHER_PHIONE);
+            response.setResult(obj);
+            return response;
+        } else if (StringUtil.equals(parsedObj.getError(), "16")) {
+            logger.error("No account detail.");
+            return errorResponse(ResponseStatus.STATUS_NO_ACCOUNT_DETAIL);
+        }
+
+        if (!StringUtil.equals(parsedObj.getError(), "0")) { // 根据token获取账户信息失败
+            logger.error("Failed to get phicomm account uid null");
+            return errorResponse(ResponseStatus.STATUS_FAILED_GET_PHICOMM_ACCOUNT);
+        }
+
+        String uid = parsedObj.getData().getUid();
+        if (StringUtil.isNullOrEmpty(uid)) {
+            logger.error("Failed to get phicomm account uid null");
+            return errorResponse(ResponseStatus.STATUS_FAILED_GET_PHICOMM_ACCOUNT);
+        }
+        logger.info("uid: {}", uid);
+        return uid;
+    }
+
+    protected PhicommAccountDetailModel getPhicommAccountByToken(String token) {
+        PhicommAccountDetailModel parsedObj = null;
+        try {
+            String accountJson = getPhicommAccountByToken(PHICOMM_ACCOUNT_URL, token);
+            logger.debug("accountJson: " + accountJson);
+            if (StringUtil.isNotEmpty(accountJson)) {
+                parsedObj = JSON.parseObject(accountJson, PhicommAccountDetailModel.class);
+            }
+        } catch (Exception e) {
+            logger.error("failed to get phicomm account ", e);
+        }
+
+        return parsedObj;
+    }
+
+    private String getPhicommAccountByToken(String phicommCloudServer, String token) {
+        Map<String, String> headerParas = new HashMap<String, String>(1);
+        headerParas.put(HTTP_HEAD_AUTHORIZATION, token);
+        try {
+            String accountJson = PhicommHttpsClient.httpsGet(phicommCloudServer,
+                    DEFAULT_CHARSET,
+                    headerParas);
+            logger.info("accountJson [{}]", accountJson);
+            return accountJson;
+        } catch (Exception e) {
+            logger.error("get phicomm account error: ", e);
+        }
+        return null;
+    }
+
+}
