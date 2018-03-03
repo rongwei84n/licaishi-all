c46587d8670535bc6b847c8a69a1019ce12daab1
ranshao2017 <zhangqh2012@163.com>
2018-03-03 14:46:18 +0800

    修改访问地址

--- licaishi-backstage/front/src/main.js
+++ licaishi-backstage/front/src/main.js
@@ -10,7 +10,7 @@ import "babel-polyfill";
 
 Vue.use(ElementUI);
 
-axios.defaults.baseURL = 'http://127.0.0.1:7074/v1';
+axios.defaults.baseURL = 'http://47.97.100.240:7074/v1';
 axios.defaults.transformRequest = [function(data) {
   return qs.stringify(data)
 }];
